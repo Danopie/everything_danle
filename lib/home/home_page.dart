8e8c1a60e41d3317bca5042096996cad40412e6c
DanLe <danle.sdev@gmail.com>
2020-04-14 01:23:51 +0700

    Fix overflow issues

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -28,10 +28,14 @@ class _HomePageState extends State<HomePage> {
         height: 32,
       ),
       AboutMe(),
+      Container(
+        height: 32,
+      ),
     ];
 
     return BasePage(
       child: ListView.builder(
+        physics: BouncingScrollPhysics(),
         padding: EdgeInsets.all(24),
         itemCount: widgets.length,
         itemBuilder: (_, index) {
@@ -45,80 +49,142 @@ class _HomePageState extends State<HomePage> {
 class PersonalOverview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height - AppHeader.HEIGHT * 2,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Spacer(
-            flex: 1,
-          ),
-          ClipOval(
-            child: Image.asset(
-              "asset/header_img.jpg",
-              height: 120,
-              width: 120,
-              fit: BoxFit.cover,
-            ),
-          ),
-          Container(
-            height: 24,
-          ),
-          Text(
-            "MY NAME IS",
-            style: TextStyles.text.s(14),
-          ),
-          Container(
-            height: 8,
-          ),
-          Text(
-            "LÊ DÂN",
-            style: TextStyles.text.superBold.s(54),
-          ),
-          Container(
-            height: 8,
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text(
-                "I'm a",
-                style: TextStyles.text.medium.s(24),
-              ),
-              Container(
-                width: 12,
-              ),
-              FadeAnimatedTextKit(
-                  isRepeatingAnimation: true,
-                  totalRepeatCount: 999,
-                  duration: Duration(seconds: 2),
-                  text: ["Mobile Developer", "Flutter Enthusiast"],
-                  textStyle: TextStyles.text.superBold.s(24).underline,
-                  textAlign: TextAlign.start,
-                  alignment:
-                      AlignmentDirectional.topStart // or Alignment.topLeft
-                  ),
-            ],
-          ),
-          Container(
-            height: 120,
-          ),
-          MyLinks(),
-          Expanded(
-            flex: 2,
-            child: Container(
-              margin: EdgeInsets.only(bottom: 24),
-              alignment: Alignment.bottomCenter,
-              child: IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.keyboard_arrow_down,
-                  size: 32,
+    return Builder(
+      builder: (BuildContext context) {
+        print('PersonalOverview.build: ${MediaQuery.of(context).size.height}');
+        if (MediaQuery.of(context).size.height > 800) {
+          return Container(
+            height: MediaQuery.of(context).size.height - AppHeader.HEIGHT * 2,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Spacer(
+                  flex: 1,
                 ),
-              ),
+                Avatar(),
+                Container(
+                  height: 24,
+                ),
+                IntroductionWords(),
+                Spacer(
+                  flex: 1,
+                ),
+                MyLinks(),
+                Expanded(
+                  flex: 2,
+                  child: GuideArrow(),
+                )
+              ],
             ),
-          )
-        ],
+          );
+        } else {
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                height: 50,
+              ),
+              Avatar(),
+              Container(
+                height: 24,
+              ),
+              IntroductionWords(),
+              Container(
+                height: 100,
+              ),
+              MyLinks(),
+              Container(
+                height: 100,
+              ),
+              GuideArrow()
+            ],
+          );
+        }
+      },
+    );
+  }
+}
+
+class GuideArrow extends StatelessWidget {
+  const GuideArrow({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 24),
+      alignment: Alignment.bottomCenter,
+      child: IconButton(
+        onPressed: () {},
+        icon: Icon(
+          Icons.keyboard_arrow_down,
+          size: 32,
+        ),
+      ),
+    );
+  }
+}
+
+class IntroductionWords extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          "MY NAME IS",
+          style: TextStyles.text.s(14),
+        ),
+        Container(
+          height: 8,
+        ),
+        Text(
+          "LÊ DÂN",
+          style: TextStyles.text.superBold.s(54),
+        ),
+        Container(
+          height: 8,
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              "I'm a",
+              style: TextStyles.text.medium.s(24),
+            ),
+            Container(
+              width: 12,
+            ),
+            FadeAnimatedTextKit(
+                isRepeatingAnimation: true,
+                totalRepeatCount: 999,
+                duration: Duration(seconds: 2),
+                text: ["Mobile Developer", "Flutter Enthusiast"],
+                textStyle: TextStyles.text.superBold.s(24).underline,
+                textAlign: TextAlign.start,
+                alignment: AlignmentDirectional.topStart // or Alignment.topLeft
+                ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class Avatar extends StatelessWidget {
+  const Avatar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipOval(
+      child: Image.asset(
+        "asset/header_img.jpg",
+        height: 120,
+        width: 120,
+        fit: BoxFit.cover,
       ),
     );
   }
@@ -127,43 +193,42 @@ class PersonalOverview extends StatelessWidget {
 class MyLinks extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final double margin = 18;
+    final divider = Container(
+      width: margin,
+    );
     final items = [
       ItemLink(
         image: "asset/logo.png",
         link: AssetLinks.github,
       ),
+      divider,
       ItemLink(
         image: "asset/medium.png",
         link: AssetLinks.medium,
       ),
+      divider,
       ItemLink(
         image: "asset/linkedin.png",
         link: AssetLinks.linkedin,
       ),
+      divider,
       ItemLink(
         image: "asset/instagram.png",
         link: AssetLinks.instagram,
       ),
+      divider,
       ItemLink(
         image: "asset/facebook.png",
         link: AssetLinks.facebook,
       ),
     ];
 
-    return Container(
-      height: 56,
-      child: ListView.separated(
-        itemCount: items.length,
-        scrollDirection: Axis.horizontal,
-        shrinkWrap: true,
-        separatorBuilder: (BuildContext context, int index) {
-          return Container(
-            width: 18,
-          );
-        },
-        itemBuilder: (BuildContext context, int index) {
-          return items[index];
-        },
+    return FittedBox(
+      fit: BoxFit.scaleDown,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: items,
       ),
     );
   }
@@ -187,6 +252,7 @@ class ItemLink extends StatelessWidget {
         padding: EdgeInsets.all(12),
         child: Image.asset(
           image,
+          height: 34,
           fit: BoxFit.contain,
         ),
       ),
@@ -211,58 +277,65 @@ class AboutMe extends StatelessWidget {
         children: [
           Expanded(
             child: Container(
-              alignment: Alignment.centerRight,
+              alignment: Alignment.topRight,
               child: Image.asset(
                 "asset/_DSC0682.png",
-                fit: BoxFit.fitHeight,
+                fit: BoxFit.cover,
               ),
             ),
           ),
           Container(
-            width: 40,
+            width: 32,
           ),
           Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    "About Me",
+                    style: TextStyles.text.superBold.s(34),
+                  ),
+                ),
                 Container(
                   height: 16,
                 ),
-                Text(
-                  "About Me",
-                  style: TextStyles.text.superBold.s(34),
-                ),
                 Expanded(
                   child: Container(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: infoMap
-                          .map((key, value) => MapEntry(
-                              key,
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Container(
-                                    margin: EdgeInsets.only(bottom: 4),
-                                    child: Text(
-                                      key,
-                                      style: TextStyles.text.bold.s(14),
+                    child: FittedBox(
+                      fit: BoxFit.scaleDown,
+                      alignment: Alignment.topLeft,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: infoMap
+                            .map((key, value) => MapEntry(
+                                key,
+                                Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.only(bottom: 4),
+                                      child: Text(
+                                        key,
+                                        style: TextStyles.text.bold.s(14),
+                                      ),
                                     ),
-                                  ),
-                                  Container(
-                                    child: SelectableText(
-                                      value,
-                                      style: TextStyles.text.light.s(20),
+                                    Container(
+                                      child: SelectableText(
+                                        value,
+                                        style: TextStyles.text.light.s(20),
+                                      ),
                                     ),
-                                  ),
-                                  Container(
-                                    height: 24,
-                                  ),
-                                ],
-                              )))
-                          .values
-                          .toList(),
+                                    Container(
+                                      height: 24,
+                                    ),
+                                  ],
+                                )))
+                            .values
+                            .toList(),
+                      ),
                     ),
                   ),
                 )
